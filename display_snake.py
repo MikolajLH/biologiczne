fixed snake new game, added yt distance normalizing

display_snake.py
@@ -21,12 +21,13 @@ class Player:
 
     
     def play(self):
-        time.sleep(1)
+        time.sleep(1)   # time to load screen
         while True:
             while gs := self.__snake.getGameStatus() == GameStatus.GAME_ON:
                 sensors = self.__snake.getSensors()
 
-                inputVector = np.array(sensors.toNormalizedList())
+                # inputVector = np.array(sensors.toNormalizedList())
+                inputVector = np.array(sensors.toNormalizedList2())
 
                 outputVector = self.__brain(inputVector)
 
@@ -34,7 +35,7 @@ class Player:
                 self.__snake.make_move(next_move)
 
                 time.sleep(self.__move_time)
-            time.sleep(1)
+            time.sleep(1)   # time between death and new game
             self.__snake.new_game()
 
 
@@ -47,7 +48,7 @@ if __name__ == "__main__":
     model.add_hidden_layer(12, relu)
     model.add_output_layer(4, softmax)
 
-    path = "uniform_biases.npz"
+    path = "sensors2.npz"   # testing snake with distance normalized as in yt video
     brain = model.copy()
     brain.load(path)
 